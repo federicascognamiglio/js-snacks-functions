1cb957ed140a92cd09685d501f1b531a19aaa8da
Federica Scognamiglio <federicascognamiglio@Federicas-Laptop.local>
2024-11-06 18:30:22 +0100

    feat: exercise 4 completed

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,20 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+/* Scrivi una funzione che accetti una stringa contenente un nome 
+e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name1 = 'Mario';
 
 // Dichiara la funzione qui.
 
+/**
+ * Funzione che restituisce un saluto seguito da un nome fornito
+ * @param {string} nameToGreet
+ * @returns {string}
+ */
+
+const greeting = (nameToGreet) => `Ciao ${nameToGreet}`;
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(greeting(name1));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
