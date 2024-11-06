fe76ce2d695faec2fe043fe09f9c8079a1f930e4
Federica Scognamiglio <federicascognamiglio@Federicas-Laptop.local>
2024-11-06 18:20:34 +0100

    feat: exercise 3 completed

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -2,12 +2,34 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
 
+/**
+ * Funzione che, data una parola, restituisce il numero di vocali al suo interno
+ * @param {string} wordToCheck
+ * @returns {number}
+ */
+
+function vowelsInWord(wordToCheck) {
+    const vowels = ["a", "e", "i", "o", "u"];
+    let wordVowelsNum = 0;
+    let arrayVowelsInWord = [];
+    for (let i = 0; i < wordToCheck.length; i++) {
+        const curChar = wordToCheck[i];
+        for (let j = 0; j < vowels.length; j++) {
+            const curVowel = vowels[j];
+            if (curChar === curVowel) {
+                wordVowelsNum += 1;
+                arrayVowelsInWord.push(curVowel);
+            }
+        }
+    }
+    return (`${wordVowelsNum} vocali (${arrayVowelsInWord})`);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const wordVowels = vowelsInWord(word);
+console.log(`La parola "${word}" contiene ${wordVowels}`);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
