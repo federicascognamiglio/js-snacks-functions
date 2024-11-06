a9ee4c9ea528fd93784f7d19479e8a72e08412fb
Federica Scognamiglio <federicascognamiglio@Federicas-Laptop.local>
2024-11-06 18:41:00 +0100

    feat: exercise 5 completed

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -1,18 +1,31 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
+/* Scrivi una funzione che accetti una stringa contenente un nome 
+e restituisca un saluto seguito dal nome fornito. 
 Il saluto deve essere :
 buongiorno se è mattina (fino alle 13), 
 buon pomeriggio se è pomeriggio (fino alle 17) 
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name1 = 'Mario';
 
 // Dichiara la funzione qui.
 
+const greeting = (nameToGreet) => {
+    const date = new Date();
+    let message;
+    if (date.getHours() <= 13) {
+        message = "Buongiorno";
+    } else if (date.getHours() <= 17) {
+        message = "Buon pomeriggio";
+    } else {
+        message = "Buonasera";
+    }
+    return `${message} ${nameToGreet}!`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const timeGreeting = greeting(name1);
+console.log(timeGreeting);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
